369f7c94391d33637a5e71128b3faf52c8ede084
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-07-11 00:00:34 +0900

    add: SSRの時刻表示ページを作成

---
+++ src/app/ssr/page.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import React, { useEffect, useState } from 'react';
+
+async function fetchCurrentTime() {
+  try {
+    const res = await fetch('http://localhost:3000/time_records');
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    const data = await res.json();
+    return data.current_time;
+  } catch (error) {
+    console.error('Fetch error:', error);
+    return null;
+  }
+}
+
+const SSRPage = () => {
+  const [currentTime, setCurrentTime] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetchCurrentTime().then(setCurrentTime);
+  }, []);
+
+  const handleFetchLatestTime = async () => {
+    const latestTime = await fetchCurrentTime();
+    setCurrentTime(latestTime);
+  };
+
+  return (
+    <div className='space-y-4'>
+      <h1>Server-Side Rendered Page</h1>
+      <p>Current Time: {currentTime ?? 'Loading...'}</p>
+      <button
+        className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+        onClick={handleFetchLatestTime}>Fetch Latest Time</button>
+    </div>
+  );
+};
+
+export default SSRPage;
